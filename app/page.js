5d8286263621b765d0e5f73a91dc3fc857a7ce7d
itsmeprinceyt <114930668+itsmeprinceyt@users.noreply.github.com>
2024-08-26 01:58:22 +0530

    testing hover down arrow

--- app/page.js
+++ app/page.js
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Main
+    <div className=" flex justify-center items-center absolute bottom-5 left-1/2">
+      <Image
+      className="transition-all duration-300 animate-pulse hover:animate-none hover:scale-110"
+      src="/icons/down-arrow.png"
+      height={50}
+      width={50}
+      alt="arrow"
+      />
     </div>
   );
 }
